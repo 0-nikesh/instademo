61679973f01c2c72040b52f7e7d868cf849765f5
Nikesh <bhandarinikesh93@gmail.com>
2023-10-12 21:17:55 +0545

    posts, followers, following

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -4,7 +4,47 @@ class UserAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Account")),
+      body: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              //profile picture
+              height: 100,
+              width: 75,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                shape: BoxShape.circle,
+              ),
+            ),
+            //number of posts, follower, following
+            Column(
+              children: [
+                Text('8',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                Text('Posts')
+              ],
+            ),
+            Column(
+              children: [
+                Text('271',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                Text('Followers')
+              ],
+            ),
+            Column(
+              children: [
+                Text('423',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                Text('Following')
+              ],
+            ),
+          ],
+        )
+      ]),
     );
   }
 }
